a9336a380f65b7435164feabf0dcbbb0db9938d2
Kristjan Tampere <dacmd@mail.ru>
2018-06-15 13:28:38 +0300

    commit

--- app/src/main/java/com/example/cmd/tictactoe/LoginFragment.kt
+++ app/src/main/java/com/example/cmd/tictactoe/LoginFragment.kt
@@ -1,13 +1,25 @@
 package com.example.cmd.tictactoe
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.cmd.tictactoe.R.layout.login_layout
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_layout.*
 
 class LoginFragment : Fragment() {
+
+    private var mAuth : FirebaseAuth? = null
+    private val userLogin : EditText? = null
+    private val userPass : EditText? = null
+
 
     companion object {
         fun newInstance() : LoginFragment {
@@ -16,7 +28,28 @@ class LoginFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(login_layout,container,false)
+        val v = inflater.inflate(login_layout,container,false)
+        mAuth = FirebaseAuth.getInstance()
+        val btn : Button ? = v.findViewById(R.id.loginLoginBtn)
+//        loginLoginBtn.setOnClickListener({loginUser()})
+        btn?.setOnClickListener({loginUser()})
+        return v
     }
+
+    private fun loginUser() {
+        val username = usernameLogin!!.text.toString()
+        val userPassword = passwordLogin!!.text.toString()
+            if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(userPassword)) {
+                mAuth?.signInWithEmailAndPassword(username, userPassword)?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(activity, ProfileActivity::class.java))
+                    }
+                }
+            } else {
+                Toast.makeText(activity, "Username or password can't be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
 }
